eb60555274c27af37c9c24f10b60ba37ad78147c
FruitWave <NictamIcii@gmail.com>
2017-10-15 15:24:31 -0700

    Formatted

--- src/Protagonist.java
+++ src/Protagonist.java
@@ -1,7 +1,6 @@
 
 import java.awt.Graphics;
 
-
 public class Protagonist extends Object_Shell {
 	int xspeed;
 	int yspeed;
@@ -10,54 +9,56 @@ public class Protagonist extends Object_Shell {
 	boolean down;
 	boolean right;
 	boolean collision;
-public Protagonist(int x, int y, int width, int height){
-	super(x,y,width,height);
-	left=false;
-	up=false;
-	down=false;
-	right=false;
-	collision=false;
-	xspeed=0;
-	yspeed=0;
-}
-public void update(){
-	if(collision){
-		
+
+	public Protagonist(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		left = false;
+		up = false;
+		down = false;
+		right = false;
+		collision = false;
+		xspeed = 0;
+		yspeed = 0;
 	}
-	else{
-	super.update();
-	if(left){
-		xspeed=-5;
+
+	public void update() {
+		if (collision) {
+
+		} else {
+			super.update();
+			if (left) {
+				xspeed = -5;
+			}
+			if (right) {
+				xspeed = 5;
+			}
+			if (up) {
+				yspeed = -5;
+			}
+			if (down) {
+				yspeed = 5;
+			}
+
+			if (!left && !right) {
+				xspeed = 0;
+			}
+			if (!up && !down) {
+				yspeed = 0;
+			}
+		}
+		// if(!up){
+		// yspeed=0;
+		// }
+		// if(!down){
+		// yspeed=0;
+		// }
+		x += xspeed;
+		y += yspeed;
+
 	}
-	if(right){
-		xspeed=5;
+
+	public void draw(Graphics g) {
+		g.drawImage(Glasspane.PlayerImg, x, y, width, height, null);
+		// test
 	}
-	if(up){
-		yspeed=-5;
-	}
-	if(down){
-		yspeed=5;
-	}
-	
-	if(!left &&!right){
-		xspeed=0;
-	}
-	if(!up&&!down){
-		yspeed=0;
-	}
-	}
-//	if(!up){
-//		yspeed=0;
-//	}
-//	if(!down){
-//		yspeed=0;
-//	}
-	x+=xspeed;
-	y+=yspeed;
-	
-}
-public void draw(Graphics g){
-	g.drawImage(Glasspane.PlayerImg, x, y, width, height, null);
-	//test
-}
 }
