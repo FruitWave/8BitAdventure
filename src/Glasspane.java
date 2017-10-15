a50481c632713571c2816d9375cb233e7c4f2f0c
FruitWave <NictamIcii@gmail.com>
2017-10-14 22:27:29 -0700

    Bug resolved!
    //uncommented 118

--- src/Glasspane.java
+++ src/Glasspane.java
@@ -115,7 +115,7 @@ public class Glasspane extends JPanel implements ActionListener, KeyListener {
 		mani = new Manipulator(this);
 		dirty = new Block(400, 200, 200, 200, 2);
 		bouncy = new Block(600, 200, 200, 200, 0);
-		// mani.addObject(bouncy);
+		 mani.addObject(bouncy);
 
 		mani.addObject(dirty);
 	}
